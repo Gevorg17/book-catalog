59ff20dd0a88217e4ec6390ec73ad635f5282a6f
Gevorg17 <45511485+Gevorg17@users.noreply.github.com>
2021-04-29 20:27:44 +0700

    header created

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import "./Header.css"
+
+function Header() {
+    return (
+        <div>
+            <header className="header">
+                <div className="name-block">
+                    <span className="name">Book Catalog</span>
+                </div>
+                <div className="auth-block">
+                    <FontAwesomeIcon className="icon" icon={faUser} size="2x"/>
+                    <a href="">Войти</a>
+                    <span>&nbsp;/&nbsp;</span>
+                    <a href="">Регистрация</a>
+                </div>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
